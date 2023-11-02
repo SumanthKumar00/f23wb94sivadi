c72a78b5adce0efb9d7a172157d338cae7210fd9
Sumanth Kumar Sivadi <sivadisumanth@gmail.com>
2023-11-01 21:17:32 -0500

    updated

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var dogRouter = require('./routes/dog');
+var boardRouter = require('./routes/board');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/dog', dogRouter);
+app.use('/board', boardRouter);
 app.use('/users', usersRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
